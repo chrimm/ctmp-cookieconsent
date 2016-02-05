94749ed59c1faf272d3bae45685c4ae4317af446
Christoffer T. Timm <kontakt@christoffertimm.de>
2016-02-05 15:03:46 +0100

    Adopted uninstall script to Settings API instead of Options API

--- uninstall.php
+++ uninstall.php
@@ -1,11 +1,15 @@
+<?php
+
 /* If uninstall is not called from WordPress, exit */
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit();
 }
 
-$option_name = 'ctmp_cookieconsent_configuration';
+$setting_names = array_keys( CTMP_Cookie_Consent::ctmpcc_default_configuration() );
 
-delete_option( $option_name );
+foreach( $setting_names as $setting_name) {
+    unregister_setting( CTMPCC_OPTION_GROUP, CTMPCC_OPTION_PREFIX.$setting_name );
+}
 
 /* For site options in Multisite */
 delete_site_option( $option_name );
