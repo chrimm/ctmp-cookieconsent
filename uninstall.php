1501d4abcc9aa87a60d00bbc3c8479f9c9a04928
Christoffer T. Timm <kontakt@christoffertimm.de>
2016-02-12 10:22:02 +0100

    Outsourced Class and fixed uninstall script

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,16 @@
 <?php
 
 /* If uninstall is not called from WordPress, exit */
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit();
 }
 
+require_once dirname(__FILE__).'/CTMP_Cookie_Consent.class.php';
+
 $setting_names = array_keys( CTMP_Cookie_Consent::ctmpcc_default_configuration() );
 
-foreach( $setting_names as $setting_name) {
+foreach( $setting_names as $setting_name ) {
+    /* Unregister and delete each setting */
     unregister_setting( CTMPCC_OPTION_GROUP, CTMPCC_OPTION_PREFIX.$setting_name );
+    delete_option( CTMPCC_OPTION_PREFIX.$setting_name );
 }
-
-/* For site options in Multisite */
-delete_site_option( $option_name );
